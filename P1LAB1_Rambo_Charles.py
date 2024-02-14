990b3cd3305ce7d9ead7ed5a68f9206d2778448c
cprambo <160037009+cprambo@users.noreply.github.com>
2024-02-14 09:48:49 -0500

    Add files via upload
    First Submission

---
+++ P1LAB1_Rambo_Charles.py
@@ -0,0 +1,12 @@
+#Charles Rambo
+#2/12/2024
+#P1LAB1
+#Using input and output
+
+#Create variable to hold first name
+first_name = input ("Enter your first name: ")
+#Print first_name with a greeting
+print ("Hey", first_name, )
+print ("Welcome to CTI-110")
+#new line \n
+
