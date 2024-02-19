1cb58e4abaf1f2c50531e31f11ca2bf51843c960
Charles Rambo <160037009+cprambo@users.noreply.github.com>
2024-02-19 16:32:15 +0000

    First upload HM2

---
+++ HW2_example.py
@@ -0,0 +1,20 @@
+#Rambo_Charles
+#Date
+#Assignment
+#Description
+'''
+Pseudocode:
+Get budget  (int)
+Get destination (str)
+Get gas (int)
+Get hotel (int)
+Get food (int)
+Add expenses 
+Budget - expenses
+Display results
+'''
+print('This program calculates and displays travel expenses')
+#get budget from user
+budget = int(input('Enter budget: '))
+dest = input('Enter your destination')
+gas = int(input('Enter gas: '))
