5bfa668a99b76c8e0a9ec445afb6599665f3e274
Charles Rambo <160037009+cprambo@users.noreply.github.com>
2024-04-24 10:02:58 -0400

    Add files via upload

---
+++ P1HW1_RamboCharles.py
@@ -0,0 +1,22 @@
+# Charles Rambo
+# 2/17/2024
+# P1HW1
+# Input and Integers
+
+number_str = input('Enter Integer: \n')
+print('You entered: ' + number_str)
+
+number_int = int(number_str)
+
+print(f'{number_int} squared is {number_int ** 2}')
+
+print(f'And {number_int} cubed is {number_int ** 3} !!')
+
+number2 = int(input('Enter another integer: \n'))
+
+print(f'{number_int} + {number2} is {number_int + number2}')
+
+print(f'{number_int} * {number2} is {number_int * number2}')
+
+
+
